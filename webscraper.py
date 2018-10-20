c9bd1c16ce2b06085f1d4b3cd3b0cdc78215595c
jcros81234 <43526992+jcros81234@users.noreply.github.com>
2018-10-20 11:46:30 -0600

    Update webscraper.py
    Changed URL to Finviz stock screener with filters "Price above $5" and "Positive for the week"
    
    Added the find all function to parse through all ticker

--- webscraper.py
+++ webscraper.py
@@ -3,8 +3,13 @@
 from bs4 import BeautifulSoup
 import requests
 
-url =  requests.get('https://www.finviz.com').text
+#grabs page
+url =  requests.get('https://finviz.com/screener.ashx?v=111&f=sh_price_o5,ta_perf_1wup&ft=3&o=company').text
 
+#xml parsing
 soup = BeautifulSoup(url, 'lxml')
 
-print(soup)
+#finds all ticker 
+ticker = soup.find_all('td', class_='screener-body-table-nw')
+
+print(ticker)
